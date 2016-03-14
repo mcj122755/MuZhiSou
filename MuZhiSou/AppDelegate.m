f0f0f6c678cedf3af7bec36ca10334102884051f
ChuanJianMu <mcj_forwork@163.com>
2016-03-14 11:27:10 +0800

    添加远程推送

--- MuZhiSou/AppDelegate.m
+++ MuZhiSou/AppDelegate.m
@@ -13,7 +13,9 @@
 #import "UMSocialWechatHandler.h"
 #import "UMSocialSinaSSOHandler.h"
 #import "UMSocialQQHandler.h"
+#import "JPUSHService.h"
 
+//极光推送appkey 43c41056e3dd28596e3f2226
 
 @interface AppDelegate ()
 
@@ -23,25 +25,25 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-//    // 注册
-//    //  自己的56d4f35867e58e7d82000ee9  友盟的53290df956240b6b4a0084b3
-//    [UMSocialData setAppKey:@"53290df956240b6b4a0084b3"];
-//    
-//    //    //设置支持没有客户端情况下使用SSO授权
-//    [UMSocialQQHandler setSupportWebView:YES];
-//    
-//    //设置微信AppId，设置分享url，默认使用友盟的网址
-//    [UMSocialWechatHandler setWXAppId:@"wxdc1e388c3822c80b" appSecret:@"a393c1527aaccb95f3a4c88d6d1455f6" url:@"http://www.umeng.com/social"];
-//    
-//    //设置分享到QQ空间的应用Id，和分享url 链接
-//    [UMSocialQQHandler setQQWithAppId:@"100424468" appKey:@"c7394704798a158208a74ab60104f0ba" url:@"http://www.umeng.com/social"];
-//    
-//    
-//    // 打开新浪微博的SSO开关
-//    // 将在新浪微博注册的应用appkey、redirectURL替换下面参数，并在info.plist的URL Scheme中相应添加wb+appkey，如"wb3921700954"，详情请参考官方文档。
-//    [UMSocialSinaSSOHandler openNewSinaSSOWithAppKey:@"3921700954" secret:@"04b48b094faeb16683c32669824ebdad" RedirectURL:@"http://sns.whalecloud.com/sina2/callback"];
-    
 
+    // 1. 添加第三方分享
+    [self addUMengShare];
+    
+    // 2. 添加极光推送
+    [self addJiGuangPush:launchOptions];
+    
+    // 添加
+    UINavigationController *navC = [[UINavigationController alloc] initWithRootViewController:[[RootViewController alloc] init]];
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = navC;
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+- (void)addUMengShare
+{
     // 注册
     //  自己的56d4f35867e58e7d82000ee9  友盟的53290df956240b6b4a0084b3
     [UMSocialData setAppKey:@"56d4f35867e58e7d82000ee9"];
@@ -59,36 +61,42 @@
     // 打开新浪微博的SSO开关
     // 将在新浪微博注册的应用appkey、redirectURL替换下面参数，并在info.plist的URL Scheme中相应添加wb+appkey，如"wb3921700954"，详情请参考官方文档。
     [UMSocialSinaSSOHandler openNewSinaSSOWithAppKey:@"1518972172" secret:@"1cf4a5cd5de3833846b2a8e9cb4929fc" RedirectURL:@"http://sns.whalecloud.com/sina2/callback"];
+}
+
+- (void)addJiGuangPush:(NSDictionary *)launchOptions
+{
+    // 解决ios8 之后接受不到消息
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+        //可以添加自定义categories
+        [JPUSHService registerForRemoteNotificationTypes:(UIUserNotificationTypeBadge |UIUserNotificationTypeSound |UIUserNotificationTypeAlert) categories:nil];
+    } else {
+        //categories 必须为nil 兼容低版本
+        [JPUSHService registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |UIRemoteNotificationTypeSound |UIRemoteNotificationTypeAlert) categories:nil];
+    }
     
-    UINavigationController *navC = [[UINavigationController alloc] initWithRootViewController:[[RootViewController alloc] init]];
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.backgroundColor = [UIColor whiteColor];
-    self.window.rootViewController = navC;
-    [self.window makeKeyAndVisible];
+    [JPUSHService setupWithOption:launchOptions appKey:@"43c41056e3dd28596e3f2226" channel:nil apsForProduction:nil];
+}
+
+// 通过设备的tocken来注册远程推送通知
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     
-    return YES;
+    // Required
+    [JPUSHService registerDeviceToken:deviceToken];
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+// 接受远程推送通知
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    
+    // Required,For systems with less than or equal to iOS6
+    [JPUSHService handleRemoteNotification:userInfo];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+// 接受到远程推送通知之后的回调函数
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    
+    // IOS 7 Support Required
+    [JPUSHService handleRemoteNotification:userInfo];
+    completionHandler(UIBackgroundFetchResultNewData);
 }
 
 @end
